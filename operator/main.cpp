ff12d72d9382427aaef2481597a4c3df0f1e5d87
xiaotao <hwxiaotao@gmail.com>
2022-08-10 21:04:34 +0700

    mod operator

--- operator/main.cpp
+++ operator/main.cpp
@@ -33,6 +33,129 @@ int main(int argc, const char * argv[]) {
     double d2 = 0.25;
     cout << d1/d2 << endl;//运算的结果也可以是小数
     
+    //取模（本质就是求余数） 只有整型变量才能做取模运算的
+    int a4 = 10;
+    int b4 = 3;
+    cout << a4 % b4 << endl;
+    
+    int a5 = 10;
+    int b5 = 20;
+    cout << a5 % b5 << endl;
+    
+    int a6 = 10;
+    int b6 = 0;
+//    cout << a6 % b6 << endl;//两个数相除除数不可以为0，所以也做不了取模运算
+    
+    double d3 = 3.14;
+    double d4 = 1.1;
+//    cout << d3/%d4 << endl; //两个小数是不可以做取模运算的
+    
+    //1、前置递增
+    int a = 10;
+    ++a;
+    cout << "a=" << a << endl;
+    //2、后置递增
+    int b = 10;
+    b++;
+    cout << "b=" << b << endl;
+    //3、前置和后置的区别
+    //前置递增 先让变量+1 然后进行表达式运算
+    int a7 = 10;
+    int b7 = ++a7 * 10;
+    cout << "a7=" << a7 << endl;
+    cout << "b7=" << b7 << endl;
+    //后置递增 先进行表达式运算，然后让变量+1
+    int a8 = 10;
+    int b8 = a8++ * 10;
+    cout << "a7=" << a8 << endl;
+    cout << "b7=" << b8 << endl;
+    
+    //赋值运算符
+    //作用：用于将表达式的值符给变量
+    
+    // =
+    int a9 = 10;
+    a9 = 100;
+    cout << "a9 = " << a9 << endl;
+    
+    // +=
+    a9 = 10;
+    a9 += 2; // a9 = a9 + 2
+    cout << "a9 = " << a9 << endl;
+    
+    // -=
+    a9 = 10;
+    a9 -= 2; // a9 = a9 - 2
+    cout << "a9 = " << a9 << endl;
+    
+    // *=
+    a9 = 10;
+    a9 *= 2; // a9 = a9 * 2
+    cout << "a9 = " << a9 << endl;
+    
+    // /=
+    a9 = 10;
+    a9 /= 2; // a9 = a9 / 2
+    cout << "a9 = " << a9 << endl;
+    
+    // %=
+    a9 = 10;
+    a9 %= 2; // a9 = a9 % 2
+    cout << "a9 = " << a9 << endl;
+    
+    //比较运算符
+    //作用：用于表达式的比较，并返回一个真值或假值
+    
+    // ==
+    cout << (a8 == b8) << endl;
+    
+    // !=
+    cout << (a8 != b8) << endl;
+    
+    // >
+    cout << (a8 > b8) << endl;
+    
+    // <
+    cout << (a8 < b8) << endl;
+    
+    // >=
+    cout << (a8 >= b8) << endl;
+    
+    // <=
+    cout << (a8 <= b8) << endl;
+    
+    //逻辑运算符
+    //作用：用于根据表达式的值返回真值或假值
+    
+    // ! 非 在C++ 除了0 都为真
+    cout << !a << endl;
+    cout << !!a << endl;
+    
+    // && 与 两个条件都为真，结果才为真(同真为真，其余为假)
+    cout << (a && b) << endl;
+    
+    a = 0;
+    b = 10;
+    cout << (a && b) << endl;
+    
+    a = 0;
+    b = 0;
+    cout << (a && b) << endl;
+    
+    // || 或（同假为假，其余为真）
+    
+    a = 10;
+    b = 10;
+    cout << (a || b) << endl;
+    
+    a = 0;
+    b = 10;
+    cout << (a || b) << endl;
+    
+    a = 0;
+    b = 0;
+    cout << (a || b) << endl;
+    
     return 0;
 }
 
---
+++ processcontrol/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  processcontrol
+//
+//  Created by mac on 2022/8/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+
+    return 0;
+}
