5576b9a38aad773c2660641f771e45aedf9bb734
xiaotao <hwxiaotao@gmail.com>
2022-08-10 14:09:03 +0700

    init pro

--- cpptutorial/main.cpp
+++ cpptutorial/main.cpp
@@ -5,10 +5,31 @@
 //  Created by mac on 2022/8/10.
 //
 
-#include <iostream>
+//创建项目
+//创建文件
+//编写代码
+//运行程序
 
+#include <iostream>
+using namespace::std;
+
+/*
+ main是一个程序的入口
+ 每个程序都必须有这么一个函数
+ 有且只有一个
+ main() 是程序开始执行的地方
+ */
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    //可以用 "\n" 代替代码里的 endl
+    cout<<"hello cpp"<<endl;
+    
+    //终止 main()函数，并向调用进程返回值 0
     return 0;
 }
+
+//单行注释
+
+/*
+ 多行注释
+ */
+
---
+++ datatype/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  datatype
+//
+//  Created by mac on 2022/8/10.
+//
+//C++规定在创建一个变量或常量时，必须要指定相应的数据类型，
+//否则无法给变量分配内存
+//数据类型存在的意义：给变量分配合适的内存空间
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    //整型
+    //作用：整型变量表示的是整数类型的数据
+    
+    return 0;
+}
